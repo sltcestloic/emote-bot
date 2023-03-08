8bca960c20bbd2230f94683a1cac21c56654e159
wshloic <lbertran@student.42lyon.fr>
2023-03-08 10:07:20 +0100

    add lolo bypass et oui j'ai les pleins pouvoirs dsl

--- src/commands/addemoji.js
+++ src/commands/addemoji.js
@@ -21,7 +21,7 @@ module.exports = {
         const name = interaction.options.getString('name');
         const url = interaction.options.getString('url');
         const userEmojis = await index.getEmojis(interaction.user.id);
-        if (userEmojis.length > 4) {
+        if (userEmojis.length > 4 && interaction.user.id != '444627882401202178') { // TODO handle emojis per server
             interaction.reply({
                 content: 'Chaque personne à le droit à 5 emojis maximum. Tu peux supprimer un de tes emojis avec la commande `/removeemoji`',
                 ephemeral: true
